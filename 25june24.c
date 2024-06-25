d23ea82e7f6aff7a17fa9c93a1cf39b5aefbcee7
pallavi thakur <thakurpallavi560@gmail.com>
2024-06-25 23:03:25 +0530

    code added

--- 25june24.c
+++ 25june24.c
@@ -1,83 +1,83 @@
 #include <stdio.h>
 //1
-// int main(){
-//     int n; int result;
-//     scanf("%d",&n);
-// result=(n%2==0)?printf("even"):printf("odd");
-// }
+int main(){
+    int n; int result;
+    scanf("%d",&n);
+result=(n%2==0)?printf("even"):printf("odd");
+}
 
 //2
-// #include <ctype.h>
-// int main(){
-//     char s;
-//     scanf("%c",&s);
+#include <ctype.h>
+int main(){
+    char s;
+    scanf("%c",&s);
     
-//     if (s=='a'||s=='e'||s=='i'||s=='o'||s=='u'){
-//         printf("vowel");
-// }
-// else{
-//     printf("consonant");
-// }
-// }
+    if (s=='a'||s=='e'||s=='i'||s=='o'||s=='u'){
+        printf("vowel");
+}
+else{
+    printf("consonant");
+}
+}
 
 //3
-// int main(){
-//     int n;
-//     scanf("%d",&n);
-//     if (n%4==0 && n%100!=0){
-//         printf("leap year");
-//     }
-//     else {
-//         printf("non leap year");
-//     }
-// }
+int main(){
+    int n;
+    scanf("%d",&n);
+    if (n%4==0 && n%100!=0){
+        printf("leap year");
+    }
+    else {
+        printf("non leap year");
+    }
+}
 
 //4 note demonstration
 
 //5 quadrant
 
 //6
-// int main(){
-//     int n,d,count=0; int digit;
-//     scanf("%d %d",&n,&d);
+int main(){
+    int n,d,count=0; int digit;
+    scanf("%d %d",&n,&d);
    
-//     int temp=n;
-//     while(temp!=0){
-//         digit=temp%10;
-//         if (digit!=d){
-//             count +=1;
-//         }
-//         temp=temp/10;
-//     } 
-//     printf("%d",count);
-// }
+    int temp=n;
+    while(temp!=0){
+        digit=temp%10;
+        if (digit!=d){
+            count +=1;
+        }
+        temp=temp/10;
+    } 
+    printf("%d",count);
+}
 
 //7
-// int main(){
-//     int n;
-//     scanf("%d",&n);
-//     int arr[n];
-//     for(int i=0;i<n;i++){
-//         scanf("%d",&arr[i]);
-//     }
-// int maxn=-1000;int maxp=0;
-// for(int i=0;i<n;i++){
-//     if (arr[i]>=0){
-//         if(arr[i]>maxp){
-//             maxp=arr[i];
-//         }
-//         }
-//         else{
-//             if(arr[i]>maxn){
-//                 maxn=arr[i];
-//             }
-//         }
-// }
-// printf("%d\n",maxp);
-// printf("%d\n",maxn);
-// printf("%d",maxp-maxn);
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+int maxn=-1000;int maxp=0;
+for(int i=0;i<n;i++){
+    if (arr[i]>=0){
+        if(arr[i]>maxp){
+            maxp=arr[i];
+        }
+        }
+        else{
+            if(arr[i]>maxn){
+                maxn=arr[i];
+            }
+        }
+}
+printf("%d\n",maxp);
+printf("%d\n",maxn);
+printf("%d",maxp-maxn);
 
-// }
+}
 
 //8 fibonacci numbers addition in array upto the given numbers 
 
