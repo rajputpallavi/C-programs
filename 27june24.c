b133bb0a17dfd255be4967e0d1e892dd3ead30e2
pallavi thakur <thakurpallavi560@gmail.com>
2024-06-27 15:34:52 +0530

    new questions

---
+++ 27june24.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+// //1
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     int temp=n; int digit; int rev=0;
+//     while(temp!=0){
+// digit=temp%10;
+// rev=10*rev+digit;
+// temp=temp/10;
+// }
+// printf("%d",n-rev);
+// }
+
+//2
+// int main(){
+//     int n; int a,b,c;
+//     scanf("%d",&n);
+//     int count=0;
+//     int temp=n;
+//     while(temp!=0){
+//     if(temp%2==0){
+//         temp=temp/2;
+//         count+=1;
+// }
+// else{
+//     temp=temp-1;
+//     count+=1;
+// }
+//     }
+//     printf("%d",count);
+// }
+
+//3
+
+
